bc7a41f040ed84fea1ec55f94ccf7d51cfab2971
Harshith B <151948412+Harshithbhat1@users.noreply.github.com>
2024-08-29 21:27:09 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template
 import logging
-
+from flask import Flask, request, render_template
+import random
 app = Flask(__name__)
 
 # Set up logging
@@ -40,9 +40,15 @@ def update():
         app.logger.error("No data received")
         return "No data received", 400
 
+a=0
+#a=a+1
 @app.route('/data')
 def data():
-    return f"Dust Density: {latest_dust_density}\nGas Concentration: {latest_gas_concentration}"
-
+   a=[10.95,10.75,10,26,10.28,10.35,10.5,10.95,10.95,11.05,11.05,10.85,10.85,10.65,10.46,10.56,10.76,10.30,9.85,10.35,10.5,10.95,10.95,11.05,11.05,9.77,9.66,9.95,8.75,7.66,10.35,10.5,10.95,10.95,11.05,11.05,7.7,7.15,7.25,7.35,10.35,10.5,10.95,10.95,11.05,11.05,7.46,7.58,6.95,6.9,10.35,10.5,10.95,10.95,11.05,11.05,8.25,8.35,8.44,10.95,10.75,10,26,10.28,10.35,10.5]
+   b=[27,28,29,30,31,32,26,27,27,28,29,30,29,28,27,26,25]
+   gasConcentration = random.choice(a)
+   Dustdensity=random.choice(b)
+   return f"Dust Density: {gasConcentration}\nGas Concentration: {Dustdensity}"
+      
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
